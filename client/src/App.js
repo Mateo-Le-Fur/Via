2ccbb7e6e1c35ede9d54d0fcd467c307ce3cd292
unknown <louisleca@zohomail.eu>
2022-08-12 16:08:03 +0200

    setup router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import './App.scss';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Landing from './pages/Landing/Landing';
+import Home from './pages/Home/Home';
+import NotFound from './pages/404/NotFound';
 
 function App() {
   return (
-    <div>
-     Hello world
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+     <Routes>
+      <Route path="/" element={<Landing />}/>
+      <Route path="/home" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+      <Route />
+     </Routes>
+     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/404/NotFound.jsx
@@ -0,0 +1,8 @@
+import "./NotFound.scss"
+
+const NotFound = () => {
+  return (
+    <div className='notFound'>NotFound</div>
+  )
+}
+export default NotFound
