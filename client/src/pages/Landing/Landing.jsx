4dca0f0420af365a29dc1cfa45326b7ad267249a
unknown <louisleca@zohomail.eu>
2022-08-12 16:49:45 +0200

    App slice setup

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,8 +1,21 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleTest } from '../../features/global/globalSlice';
 import "./Landing.scss";
 
 const Landing = () => {
+  const dispatch = useDispatch()
+  const {test} = useSelector((state) => state.global)
+
+  
   return (
-    <div className='landing'>Landing</div>
+    <div className='landing'>
+      Landing
+      <div>
+      <button onClick={() => dispatch(toggleTest())}>Test reducer</button>
+      {test && <div>Hello</div>}
+      </div>
+      
+    </div>
   )
 }
 export default Landing
