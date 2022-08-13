99037e28376de5985e9bf3e0cea657283abbfe7c
unknown <louisleca@zohomail.eu>
2022-08-13 13:48:37 +0200

    Leaflet setup

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,29 +1,32 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { toggleTest } from '../../features/global/globalSlice';
-import { useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import './Landing.scss';
-import axios from "axios"
 
 const Landing = () => {
-  const dispatch = useDispatch();
-  const { test } = useSelector((state) => state.global);
-
-  useEffect(() => {
-    const login = async () => {
-      const res = await axios.get("/login")
-      console.log(res.data)
-    }
-
-    login()
-  }, []);
-
+  const center = [51.505, -0.09];
+  const positions = [
+    [51.505, -0.09],
+    [51.33, -0.08],
+    [51.23, -0.098],
+    [51.13, -0.038],
+    [51.33, -0.082],
+    [51.23, -0.028],
+    [51.63, -0.068],
+  ];
   return (
     <div className='landing'>
-      Landing
-      <div>
-        <button onClick={() => dispatch(toggleTest())}>Test reducer</button>
-        {test && <div>Hello</div>}
-      </div>
+      <MapContainer center={center} zoom={13} scrollWheelZoom={true} attributionControl={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        {positions.map((position, index) => (
+          <Marker key={index} position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 };
