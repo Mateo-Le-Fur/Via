34f74fb9c077b015c30a038db44b898e9da04199
unknown <louisleca@zohomail.eu>
2022-08-13 10:36:51 +0200

    json-server setup

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,21 +1,30 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleTest } from '../../features/global/globalSlice';
-import "./Landing.scss";
+import { useEffect } from 'react';
+import './Landing.scss';
+import axios from "axios"
 
 const Landing = () => {
-  const dispatch = useDispatch()
-  const {test} = useSelector((state) => state.global)
+  const dispatch = useDispatch();
+  const { test } = useSelector((state) => state.global);
 
-  
+  useEffect(() => {
+    const login = async () => {
+      const res = await axios.get("/login")
+      console.log(res.data)
+    }
+
+    login()
+  }, []);
+
   return (
     <div className='landing'>
       Landing
       <div>
-      <button onClick={() => dispatch(toggleTest())}>Test reducer</button>
-      {test && <div>Hello</div>}
+        <button onClick={() => dispatch(toggleTest())}>Test reducer</button>
+        {test && <div>Hello</div>}
       </div>
-      
     </div>
-  )
-}
-export default Landing
+  );
+};
+export default Landing;
