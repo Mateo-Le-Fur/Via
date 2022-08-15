4e0e499ba843338feff44d68d6c23e22bacb0f7d
unknown <louisleca@zohomail.eu>
2022-08-15 18:07:16 +0200

    Landing layout

---
+++ client/src/pages/Landing/LandingForm.jsx
@@ -0,0 +1,34 @@
+import Login from "./Forms/Login";
+import Register from "./Forms/Register";
+import {useState} from "react";
+import "./Landing.scss";
+
+const LandingForm = () => {
+    const [isMember, setIsMember] = useState(true)
+    const toggleMember = () => {
+      setIsMember(!isMember)
+    }
+  return (
+    <div className="landingForm">
+      <div className="container">
+        <div className="logo">
+          <img src='/via.png' alt="" />
+        </div>
+        <div className="form">
+          {isMember ? (
+            <Login />
+          ) : (
+            <Register />
+          )}
+        </div>
+      </div>
+      <p className='isMember'>
+        {isMember ? "Pas encore inscrit ?" : "Vous avez déjà un compte ?"}
+        <button type="button" onClick={toggleMember} className="member-btn">
+          {isMember ? "S'inscrire" : "Se connecter"}
+        </button>
+      </p>
+    </div>
+  )
+}
+export default LandingForm
