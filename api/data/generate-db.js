d8546dc8cae2eb6f632a8eba30bff94f6d8997a5
Leomhann <ithilsin@yahoo.fr>
2022-08-15 15:12:18 +0200

    Data generation with FakerJs done

--- api/data/generate-db.js
+++ api/data/generate-db.js
@@ -1,8 +1,28 @@
 const { faker } = require('@faker-js/faker');
 
-function generateUsers(userNb) {
-  const users = [];
+let users = [];
+let activities = [];
+let messages = [];
+let comments = [];
+const baseTypes = [
+  'Arts',
+  'Activités créatives',
+  'Apéro',
+  'Bénévolat',
+  'Cinéma',
+  'Cuisine',
+  'Danse',
+  'Jardinage', 
+  'Jeux',
+  'Bricolage'
+]
+let types = [];
+let bookmarks = [];
+let participations = [];
+let activityTypes = [];
 
+// Generate users and add them to the database table "user"
+function generateUsers(userNb) {
   for (let i = 0 ; i < userNb ; i++ ) {
     const user = {
       email: faker.internet.email(),
@@ -23,9 +43,8 @@ function generateUsers(userNb) {
   return users;
 }
 
+// Generate activities and add them to the database table "activity"
 function generateActivities(activityNb) {
-  const activities = [];
-
   for (let i = 0 ; i < activityNb ; i++ ) {
     const activity = {
       name: faker.name.jobTitle(),
@@ -35,7 +54,7 @@ function generateActivities(activityNb) {
       city: faker.address.cityName(),
       lat: faker.address.latitude(),
       long: faker.address.longitude(),
-      user_id: faker.datatype.number({min: 1})
+      user_id: users.indexOf(users[Math.floor(Math.random() * users.length)])+1
     };
   
     activities.push(activity);
@@ -43,7 +62,95 @@ function generateActivities(activityNb) {
   return activities;
 }
 
+// Generate messages and add them to the database table "message"
+function generateMessages(messageNb) {
+  for (let i = 0 ; i < messageNb ; i++ ) {
+    const message = {
+      text: faker.lorem.paragraph(number = 3, string = ' '),
+      sender: users.indexOf(users[Math.floor(Math.random() * users.length)])+1,
+      receiver: users.indexOf(users[Math.floor(Math.random() * users.length)])+1
+    };
+  
+    messages.push(message);
+  }
+  return messages;
+}
+
+// Generate comments and add them to the database table "comment"
+function generateComments(commentNb) {
+  for (let i = 0 ; i < commentNb ; i++ ) {
+    const comment = {
+      text: faker.lorem.paragraph(number = 3, string = ' '),
+      user_id: users.indexOf(users[Math.floor(Math.random() * users.length)])+1,
+      activity_id: activities.indexOf(activities[Math.floor(Math.random() * activities.length)])+1
+    };
+  
+    comments.push(comment);
+  }
+  return comments;
+}
+
+// Generate types and add them to the database table "type"
+function generateTypes() {
+  for (let i = 0 ; i < baseTypes.length ; i++ ) {
+    const type = {
+      label: baseTypes[i],
+    };
+  
+    types.push(type);
+  }
+  return types;
+}
+
+// Generate bookmarks from users and add them to the database table "user_has_activity"
+function generateUserBookmarks(bookmarkNb) {
+  for (let i = 0 ; i < bookmarkNb ; i++ ) {
+    const bookmark = {
+      user_id: users.indexOf(users[Math.floor(Math.random() * users.length)])+1,
+      activity_id: activities.indexOf(activities[Math.floor(Math.random() * activities.length)])+1
+    };
+  
+    bookmarks.push(bookmark);
+  }
+  return bookmarks;
+}
+
+// Generate participations to activity and add them to the database table "user_to_activity"
+function generateUserParticipations(participationNb) {
+  for (let i = 0 ; i < participationNb ; i++ ) {
+    const participation = {
+      user_id: users.indexOf(users[Math.floor(Math.random() * users.length)])+1,
+      activity_id: activities.indexOf(activities[Math.floor(Math.random() * activities.length)])+1
+    };
+  
+    participations.push(participation);
+  }
+  return participations;
+}
+
+// TODO Affiner l'assignation des types aux activités (éviter de se retrouver avec une activité ayant des types contradictoires ou incohérents)
+// Generate types belongin to activity and add them to the database table "activity_has_type"
+function generateUserActivityTypes(activityTypeNb) {
+  for (let i = 0 ; i < activityTypeNb ; i++ ) {
+    const activityType = {
+      type_id: types.indexOf(types[Math.floor(Math.random() * types.length)])+1,
+      activity_id: activities.indexOf(activities[Math.floor(Math.random() * activities.length)])+1
+    };
+  
+    activityTypes.push(activityType);
+  }
+  return activityTypes;
+}
+
 (() => {
-  console.log(generateUsers(1));
-  console.log(generateActivities(1));
+  generateUsers(10);
+  generateActivities(5);
+  generateMessages(5);
+  generateComments(5);
+  generateTypes();
+  generateUserBookmarks(5);
+  generateUserParticipations(5);
+  generateUserActivityTypes(5);
+
+  console.log(activityTypes);
 })();
