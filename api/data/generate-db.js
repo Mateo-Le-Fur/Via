d413dd25f190a72f7e8d31d26b98a5c281e85de4
Leomhann <ithilsin@yahoo.fr>
2022-08-16 15:22:07 +0200

    Random real addresses generation for DB seeding

--- api/data/generate-db.js
+++ api/data/generate-db.js
@@ -1,9 +1,55 @@
+require('dotenv').config();
+const client = require('../app/config/pg');
 const { faker } = require('@faker-js/faker');
+const axios = require('axios');
+const addressData = require('./address-data.json');
 
-let users = [];
-let activities = [];
-let messages = [];
-let comments = [];
+const streetNumbers = [];
+const streets = [];
+const postalCodes = [];
+
+addressData.forEach(address => {
+  streets.push(address.name);
+  postalCodes.push(address.postcode);
+});
+
+const newAddress = addressData.map(address => {
+  return {
+    street: address.name,
+    postalCode: address.postcode,
+    city: address.city[0]
+  }
+});
+
+function randomAddress() {
+  const rand = Math.floor(Math.random() * newAddress.length);
+
+  const randAddress = {
+    nb: 15,
+    street: newAddress[rand].street,
+    postalCode: newAddress[rand].postalCode,
+    city: newAddress[rand].city
+  }
+
+  return randAddress;
+};
+
+// console.log(randomAddress());
+
+faker.locale = 'fr';
+
+const userNb = 20;
+const activityNb = 25;
+const messageNb = 0;
+const commentNb = 0;
+const bookmarkNb = 0;
+const participationNb = 0;
+const activityTypeNb = 0;
+
+const users = [];
+const activities = [];
+const messages = [];
+const comments = [];
 const baseTypes = [
   'Arts',
   'Activités créatives',
@@ -16,10 +62,24 @@ const baseTypes = [
   'Jeux',
   'Bricolage'
 ]
-let types = [];
-let bookmarks = [];
-let participations = [];
-let activityTypes = [];
+const types = [];
+const bookmarks = [];
+const participations = [];
+const activityTypes = [];
+
+const city = 'Paris';
+
+function pgQuoteEscape(row) {
+  const newRow = {};
+  Object.entries(row).forEach(([prop, value]) => {
+      if (typeof value !== 'string') {
+          newRow[prop] = value;
+          return;
+      }
+      newRow[prop] = value.replaceAll("'", "''");
+  });
+  return newRow;
+}
 
 // Generate users and add them to the database table "user"
 function generateUsers(userNb) {
@@ -27,12 +87,12 @@ function generateUsers(userNb) {
     const user = {
       email: faker.internet.email(),
       password: faker.internet.password(),
-      nickname: faker.lorem.word(),
+      nickname: faker.name.middleName(),
       firstname: faker.name.firstName(),
       lastname: faker.name.lastName(),
       description: faker.lorem.paragraph(number = 2, string = ' '),
-      address: faker.address.streetAddress(false),
-      city: faker.address.cityName(),
+      address: `${randomAddress().nb} ${randomAddress().street} ${randomAddress().postalCode}`,
+      city: randomAddress().city,
       phone: faker.phone.number('06 ## ## ## ##', string),
       avatar: faker.image.people(400, 400),
       is_admin: false,
@@ -43,15 +103,58 @@ function generateUsers(userNb) {
   return users;
 }
 
+async function insertUsers(users) {
+  await client.query('TRUNCATE TABLE "user" RESTART IDENTITY CASCADE');
+
+  const userValues = users.map((user) => {
+      const newUser = pgQuoteEscape(user);
+      return `(
+          '${newUser.email}',
+          '${newUser.password}',
+          '${newUser.nickname}',
+          '${newUser.firstname}',
+          '${newUser.lastname}',
+          '${newUser.description}',
+          '${newUser.address}',
+          '${newUser.city}',
+          '${newUser.phone}',
+          '${newUser.avatar}',
+          '${newUser.is_admin}'
+      )`;
+  });
+
+  const queryStr = `
+           INSERT INTO "user"
+           (
+            "email",
+            "password",
+            "nickname",
+            "firstname",
+            "lastname",
+            "description",
+            "address",
+            "city",
+            "phone",
+            "avatar",
+            "is_admin"
+           )
+           VALUES
+           ${userValues}
+           RETURNING id
+   `;
+  const result = await client.query(queryStr);
+  return result.rows;
+}
+
 // Generate activities and add them to the database table "activity"
 function generateActivities(activityNb) {
   for (let i = 0 ; i < activityNb ; i++ ) {
     const activity = {
-      name: faker.name.jobTitle(),
+      name: faker.lorem.sentence(),
       description: faker.lorem.paragraph(number = 2, string = ' '),
-      date: faker.date.future(1),
-      address: faker.address.streetAddress(false),
-      city: faker.address.cityName(),
+      date: faker.date.future(0.5),
+      address: `${randomAddress().nb} ${randomAddress().street} ${randomAddress().postalCode}`,
+      city: randomAddress().city,
       lat: faker.address.latitude(),
       long: faker.address.longitude(),
       user_id: users.indexOf(users[Math.floor(Math.random() * users.length)])+1
@@ -62,32 +165,40 @@ function generateActivities(activityNb) {
   return activities;
 }
 
-// Generate messages and add them to the database table "message"
-function generateMessages(messageNb) {
-  for (let i = 0 ; i < messageNb ; i++ ) {
-    const message = {
-      text: faker.lorem.paragraph(number = 3, string = ' '),
-      sender: users.indexOf(users[Math.floor(Math.random() * users.length)])+1,
-      receiver: users.indexOf(users[Math.floor(Math.random() * users.length)])+1
-    };
-  
-    messages.push(message);
-  }
-  return messages;
-}
+async function insertActivities(activities) {
+  await client.query('TRUNCATE TABLE "activity" RESTART IDENTITY CASCADE');
 
-// Generate comments and add them to the database table "comment"
-function generateComments(commentNb) {
-  for (let i = 0 ; i < commentNb ; i++ ) {
-    const comment = {
-      text: faker.lorem.paragraph(number = 3, string = ' '),
-      user_id: users.indexOf(users[Math.floor(Math.random() * users.length)])+1,
-      activity_id: activities.indexOf(activities[Math.floor(Math.random() * activities.length)])+1
-    };
-  
-    comments.push(comment);
-  }
-  return comments;
+  const activityValues = activities.map((activity) => {
+      const newActivity = pgQuoteEscape(activity);
+      return `(
+          '${newActivity.name}',
+          '${newActivity.description}',
+          '${newActivity.date}',
+          '${newActivity.address}',
+          '${newActivity.city}',
+          '${newActivity.lat}',
+          '${newActivity.long}',
+          '${newActivity.user_id}'
+      )`;
+  });
+  const queryStr = `
+           INSERT INTO "activity"
+           (
+            "name",
+            "description",
+            "date",
+            "address",
+            "city",
+            "lat",
+            "long",
+            "user_id"
+           )
+           VALUES
+           ${activityValues}
+           RETURNING id
+   `;
+  const result = await client.query(queryStr);
+  return result.rows;
 }
 
 // Generate types and add them to the database table "type"
@@ -102,55 +213,206 @@ function generateTypes() {
   return types;
 }
 
-// Generate bookmarks from users and add them to the database table "user_has_activity"
-function generateUserBookmarks(bookmarkNb) {
-  for (let i = 0 ; i < bookmarkNb ; i++ ) {
-    const bookmark = {
-      user_id: users.indexOf(users[Math.floor(Math.random() * users.length)])+1,
-      activity_id: activities.indexOf(activities[Math.floor(Math.random() * activities.length)])+1
-    };
-  
-    bookmarks.push(bookmark);
-  }
-  return bookmarks;
+async function insertTypes(types) {
+  await client.query('TRUNCATE TABLE "type" RESTART IDENTITY CASCADE');
+
+  const typeValues = types.map((type) => {
+      const newType = pgQuoteEscape(type);
+      return `(
+          '${newType.label}'
+      )`;
+  });
+  const queryStr = `
+           INSERT INTO "type"
+           (
+            "label"
+           )
+           VALUES
+           ${typeValues}
+           RETURNING id
+   `;
+  const result = await client.query(queryStr);
+  return result.rows;
 }
 
-// Generate participations to activity and add them to the database table "user_to_activity"
-function generateUserParticipations(participationNb) {
-  for (let i = 0 ; i < participationNb ; i++ ) {
-    const participation = {
-      user_id: users.indexOf(users[Math.floor(Math.random() * users.length)])+1,
-      activity_id: activities.indexOf(activities[Math.floor(Math.random() * activities.length)])+1
-    };
+// // Generate messages and add them to the database table "message"
+// function generateMessages(messageNb) {
+//   for (let i = 0 ; i < messageNb ; i++ ) {
+//     const message = {
+//       text: faker.lorem.paragraph(number = 3, string = ' '),
+//       sender: users.indexOf(users[Math.floor(Math.random() * users.length)])+1,
+//       receiver: users.indexOf(users[Math.floor(Math.random() * users.length)])+1
+//     };
   
-    participations.push(participation);
-  }
-  return participations;
-}
+//     messages.push(message);
+//   }
+//   return messages;
+// }
 
-// TODO Affiner l'assignation des types aux activités (éviter de se retrouver avec une activité ayant des types contradictoires ou incohérents)
-// Generate types belongin to activity and add them to the database table "activity_has_type"
-function generateUserActivityTypes(activityTypeNb) {
-  for (let i = 0 ; i < activityTypeNb ; i++ ) {
-    const activityType = {
-      type_id: types.indexOf(types[Math.floor(Math.random() * types.length)])+1,
-      activity_id: activities.indexOf(activities[Math.floor(Math.random() * activities.length)])+1
-    };
+// async function insertMessages(messages) {
+//   await client.query('TRUNCATE TABLE "message" RESTART IDENTITY CASCADE');
+
+//   const messageValues = messages.map((message) => {
+//       // const newMessage = pgQuoteEscape(message);
+//       return `(
+//           '${message.text}',
+//           '${message.sender}',
+//           '${message.receiver}'
+//       )`;
+//   });
+//   const queryStr = `
+//            INSERT INTO "message"
+//            (
+//             "text",
+//             "sender",
+//             "receiver"
+//            )
+//            VALUES
+//            ${messageValues}
+//            RETURNING id
+//    `;
+//   const result = await client.query(queryStr);
+//   return result.rows;
+// }
+
+// // Generate comments and add them to the database table "comment"
+// function generateComments(commentNb) {
+//   for (let i = 0 ; i < commentNb ; i++ ) {
+//     const comment = {
+//       text: faker.lorem.paragraph(number = 3, string = ' '),
+//       user_id: users.indexOf(users[Math.floor(Math.random() * users.length)])+1,
+//       activity_id: activities.indexOf(activities[Math.floor(Math.random() * activities.length)])+1
+//     };
   
-    activityTypes.push(activityType);
-  }
-  return activityTypes;
-}
+//     comments.push(comment);
+//   }
+//   return comments;
+// }
 
-(() => {
-  generateUsers(10);
-  generateActivities(5);
-  generateMessages(5);
-  generateComments(5);
-  generateTypes();
-  generateUserBookmarks(5);
-  generateUserParticipations(5);
-  generateUserActivityTypes(5);
+// async function insertComments(messages) {
+//   await client.query('TRUNCATE TABLE "comment" RESTART IDENTITY CASCADE');
 
-  console.log(activityTypes);
+//   const commentValues = comments.map((comment) => {
+//       // const newComment = pgQuoteEscape(comment);
+//       return `(
+//           '${comment.text}',
+//           '${comment.user_id}',
+//           '${comment.activity_id}'
+//       )`;
+//   });
+//   const queryStr = `
+//            INSERT INTO "message"
+//            (
+//             "text",
+//             "user_id",
+//             "activity_id"
+//            )
+//            VALUES
+//            ${commentValues}
+//            RETURNING id
+//    `;
+//   const result = await client.query(queryStr);
+//   return result.rows;
+// }
+
+// // Generate bookmarks from users and add them to the database table "user_has_activity"
+// function generateUserBookmarks(bookmarkNb) {
+//   for (let i = 0 ; i < bookmarkNb ; i++ ) {
+//     const bookmark = {
+//       user_id: users.indexOf(users[Math.floor(Math.random() * users.length)])+1,
+//       activity_id: activities.indexOf(activities[Math.floor(Math.random() * activities.length)])+1
+//     };
+  
+//     bookmarks.push(bookmark);
+//   }
+//   return bookmarks;
+// }
+
+// // Generate participations to activity and add them to the database table "user_to_activity"
+// function generateUserParticipations(participationNb) {
+//   for (let i = 0 ; i < participationNb ; i++ ) {
+//     const participation = {
+//       user_id: users.indexOf(users[Math.floor(Math.random() * users.length)])+1,
+//       activity_id: activities.indexOf(activities[Math.floor(Math.random() * activities.length)])+1
+//     };
+  
+//     participations.push(participation);
+//   }
+//   return participations;
+// }
+
+// // TODO Affiner l'assignation des types aux activités (éviter de se retrouver avec une activité ayant des types contradictoires ou incohérents)
+// // Generate types belongin to activity and add them to the database table "activity_has_type"
+// function generateUserActivityTypes(activityTypeNb) {
+//   for (let i = 0 ; i < activityTypeNb ; i++ ) {
+//     const activityType = {
+//       type_id: types.indexOf(types[Math.floor(Math.random() * types.length)])+1,
+//       activity_id: activities.indexOf(activities[Math.floor(Math.random() * activities.length)])+1
+//     };
+  
+//     activityTypes.push(activityType);
+//   }
+//   return activityTypes;
+// }
+
+// function getAdress() {
+//   $("#inputRue").keyup(function(event) {
+//     // Stop la propagation par défaut
+//           event.preventDefault();
+//           event.stopPropagation();
+  
+//           let rue = $("#inputRue").val();
+//           $.get('https://api-adresse.data.gouv.fr/search/', {
+//               q: rue,
+//               limit: 15,
+//               autocomplete: 1
+//           }, function (data, status, xhr) {
+//               let liste = "";
+//               $.each(data.features, function(i, obj) {
+//                   console.log(obj.properties);
+//                   // données phase 1 (obj.properties.label) & phase 2 : name, postcode, city
+//                   // J'ajoute chaque élément dans une liste
+//                   liste += '<li><a href="#" name="'+obj.properties.label+'" data-name="'+obj.properties.name+'" data-postcode="'+obj.properties.postcode+'" data-city="'+obj.properties.city+'">'+obj.properties.label+'</a></li>';
+//               });
+//               $('.adress-feedback ul').html(liste);
+  
+//               // ToDo: Au clic du lien voulu, on envoie l'info en $_POST
+//               $('.adress-feedback ul>li').on("click","a", function(event) {
+//                   // Stop la propagation par défaut
+//                   event.preventDefault();
+//                   event.stopPropagation();
+  
+//                   let adresse = $(this).attr("name");
+  
+//                   $("#inputRue").val($(this).attr("data-name"));
+//                   $("#inputCodePostal").val($(this).attr("data-postcode"));
+//                   $("#inputVille").val($(this).attr("data-city"));
+  
+//                   $('.adress-feedback ul').empty();
+//               });
+  
+//           }).error(function () {
+//               // alert( "error" );
+//           }).always(function () {
+//               // alert( "finished" );
+//           }, 'json');
+//       });
+// }
+
+(async () => {
+  const generatedUsers = generateUsers(userNb);
+  const generatedActivities = generateActivities(activityNb);
+  const generatedTypes = generateTypes();
+
+  // generateMessages(messageNb);
+  // generateComments(commentNb);
+  // generateUserBookmarks(bookmarkNb);
+  // generateUserParticipations(participationNb);
+  // generateUserActivityTypes(activityTypeNb);
+
+  const usersData = await insertUsers(users);
+  const activitiesData = await insertActivities(activities);
+  const typesData = await insertTypes(types);
+
+  client.end();
 })();
