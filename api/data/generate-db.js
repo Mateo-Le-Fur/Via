af22845b451b461e8cf34104f599f3fe0bc8ca1d
Leomhann <ithilsin@yahoo.fr>
2022-08-16 15:46:30 +0200

    Random address generation for db seeding

--- api/data/generate-db.js
+++ api/data/generate-db.js
@@ -8,10 +8,10 @@ const streetNumbers = [];
 const streets = [];
 const postalCodes = [];
 
-addressData.forEach(address => {
-  streets.push(address.name);
-  postalCodes.push(address.postcode);
-});
+// addressData.forEach(address => {
+//   streets.push(address.name);
+//   postalCodes.push(address.postcode);
+// });
 
 const newAddress = addressData.map(address => {
   return {
@@ -33,8 +33,6 @@ function randomAddress() {
 
   return randAddress;
 };
-
-// console.log(randomAddress());
 
 faker.locale = 'fr';
 
@@ -353,50 +351,6 @@ async function insertTypes(types) {
 //     activityTypes.push(activityType);
 //   }
 //   return activityTypes;
-// }
-
-// function getAdress() {
-//   $("#inputRue").keyup(function(event) {
-//     // Stop la propagation par défaut
-//           event.preventDefault();
-//           event.stopPropagation();
-  
-//           let rue = $("#inputRue").val();
-//           $.get('https://api-adresse.data.gouv.fr/search/', {
-//               q: rue,
-//               limit: 15,
-//               autocomplete: 1
-//           }, function (data, status, xhr) {
-//               let liste = "";
-//               $.each(data.features, function(i, obj) {
-//                   console.log(obj.properties);
-//                   // données phase 1 (obj.properties.label) & phase 2 : name, postcode, city
-//                   // J'ajoute chaque élément dans une liste
-//                   liste += '<li><a href="#" name="'+obj.properties.label+'" data-name="'+obj.properties.name+'" data-postcode="'+obj.properties.postcode+'" data-city="'+obj.properties.city+'">'+obj.properties.label+'</a></li>';
-//               });
-//               $('.adress-feedback ul').html(liste);
-  
-//               // ToDo: Au clic du lien voulu, on envoie l'info en $_POST
-//               $('.adress-feedback ul>li').on("click","a", function(event) {
-//                   // Stop la propagation par défaut
-//                   event.preventDefault();
-//                   event.stopPropagation();
-  
-//                   let adresse = $(this).attr("name");
-  
-//                   $("#inputRue").val($(this).attr("data-name"));
-//                   $("#inputCodePostal").val($(this).attr("data-postcode"));
-//                   $("#inputVille").val($(this).attr("data-city"));
-  
-//                   $('.adress-feedback ul').empty();
-//               });
-  
-//           }).error(function () {
-//               // alert( "error" );
-//           }).always(function () {
-//               // alert( "finished" );
-//           }, 'json');
-//       });
 // }
 
 (async () => {
