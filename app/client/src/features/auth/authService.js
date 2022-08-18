ee297a442afca6f5f981fa2cb976d396cbd786d4
unknown <louisleca@zohomail.eu>
2022-08-18 15:59:26 +0200

    fix action checkUser

--- app/client/src/features/auth/authService.js
+++ app/client/src/features/auth/authService.js
@@ -4,14 +4,12 @@ import { privateReq, publicReq } from '../../utils/axiosMethod'
 // Register user
 const register = async (userData) => {
   const response = await publicReq.post('/auth/register', userData)
-  console.log(response)
           return response.data 
 }
 
 // Login user
 const login = async (userData) => {
   const response = await publicReq.post('/auth/login', userData)
-  console.log(response)
     return response.data
 }
 
@@ -19,14 +17,12 @@ const login = async (userData) => {
 // Checkout user
 const checkUser = async () => {
   const response = await privateReq.get('/auth/current')
-  console.log(response)
     return response.data
 
 }
 
 // Logout user
 const logout = async () => {
-
   const response = await privateReq.get('/auth/logout')
   console.log(response)
   return response.data
