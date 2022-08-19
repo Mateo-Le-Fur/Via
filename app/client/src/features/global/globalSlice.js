233718cebf86622096c3f8cf62e0a1160c416bf5
unknown <louisleca@zohomail.eu>
2022-08-19 16:25:35 +0200

    sidebar display

--- app/client/src/features/global/globalSlice.js
+++ app/client/src/features/global/globalSlice.js
@@ -1,19 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    test: true
+    showSidebar: false,
+    panel: "",
+    showFilter: false,
+    filter: "",
 }
 
 const globalSlice = createSlice({
     name: "global",
     initialState,
     reducers: {
-        toggleTest: (state) => {
-            state.test = !state.test
+        handleShowSidebar: (state) => {
+            state.showSidebar = true
+        },
+        activePanel: (state, action) => {
+            state.panel = action.payload
+        },
+        handleShowFilter: (state) => {
+            state.showFilter = !state.showFilter
+        },
+        activeFilter: (state, action) => {
+            state.filter = action.payload
         }
     }
 })
 
-export const {toggleTest} = globalSlice.actions
+export const {handleShowSidebar, activePanel, handleShowFilter, activeFilter} = globalSlice.actions
 
 export default globalSlice.reducer
--- app/client/src/pages/Home/Home.jsx
+++ app/client/src/pages/Home/Home.jsx
@@ -1,16 +1,23 @@
-import { useDispatch } from 'react-redux'
-
-import { logout } from '../../features/auth/authSlice'
+import { Marker, Popup } from 'react-leaflet'
+import { useSelector } from 'react-redux'
+import Map from '../../components/Map/Map'
 import "./Home.scss"
+import Sidebar from './Sidebar/Sidebar'
 
 const Home = () => {
-  const dispatch = useDispatch()
-
+  const {user} = useSelector(state => state.auth)
   return (
     <div className='home'>
-      <button onClick={() =>   dispatch(logout())}>
-        Logout
-      </button>
+    {user && (
+      <Map center={[user.lat, user.long]} zoom={13} zoomControl={true}>
+      <Marker  position={[user.lat, user.long]}>
+            <Popup>
+              Hello {user.nickname} you are in {user.city }
+            </Popup>
+        </Marker>
+      </Map>
+    )}
+    <Sidebar />
     </div>
   )
 }
