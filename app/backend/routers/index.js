5056bb6403ac9014e7aad13a9352993fce764766
Mateo-Le-Fur <mateo.le-fur@hotmail.com>
2022-08-22 17:26:37 +0200

    protection added on user and dashboard controllers

--- app/backend/routers/index.js
+++ app/backend/routers/index.js
@@ -1,15 +1,17 @@
 const router = require('express').Router();
 const authJWT = require('../middleware/jwt');
+const authAdmin = require('../middleware/admin');
 const auth = require('./auth');
 const user = require('./user');
 const activity = require('./activity');
 const dashboard = require('./dashboard');
+const controllerHandler = require('../helpers/controllerHandler');
 
 router.use('/auth', auth);
-router.use('/user', authJWT.protect, user);
-router.use('/activity', authJWT.protect, activity);
-router.use('/dashboard', authJWT.protect, dashboard);
-router.use('/current', authJWT.protect, (req, res) => {
+router.use('/user', controllerHandler(authJWT.protect), user);
+router.use('/activity', controllerHandler(authJWT.protect), activity);
+router.use('/dashboard', controllerHandler(authJWT.protect), controllerHandler(authAdmin.protect), dashboard);
+router.use('/current', controllerHandler(authJWT.protect), (req, res) => {
   res.json(req.user);
 });
 
