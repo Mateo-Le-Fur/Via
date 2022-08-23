ffe8e29ea4cd4b6c2cff63c4e3350b29a0b48042
Leomhann <ithilsin@yahoo.fr>
2022-08-23 14:23:29 +0200

    Refact SwaggerDoc

--- app/backend/routers/index.js
+++ app/backend/routers/index.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const authJWT = require("../middleware/jwt");
+const adminCheck = require("../middleware/admin");
 const auth = require("./auth");
 const user = require("./user");
 const activity = require("./activity");
@@ -9,11 +10,10 @@ const apiDoc = require("./apiDoc");
 router.use("/auth", auth);
 router.use("/user", authJWT.protect, user);
 router.use("/activity", authJWT.protect, activity);
-router.use("/dashboard", authJWT.protect, dashboard);
+router.use("/dashboard", authJWT.protect, adminCheck.protect, dashboard);
 router.use("/current", authJWT.protect, (req, res) => {
-
   res.json(req.user);
 });
-router.use("/docs", authJWT.protect, apiDoc);
+router.use("/docs", authJWT.protect, adminCheck.protect, apiDoc);
 
 module.exports = router;
