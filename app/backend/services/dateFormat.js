6411ec9d81e614b383f60ec9665ca10e1b81170c
Leomhann <ithilsin@yahoo.fr>
2022-08-24 15:04:26 +0200

    Date formating service

---
+++ app/backend/services/dateFormat.js
@@ -0,0 +1,9 @@
+const dayjs = require("dayjs");
+require("dayjs/locale/fr");
+
+module.exports = {
+  convertActivityDate(activity) {
+    const date = dayjs(activity.date).locale("fr").format("dd/MM/YYYY");
+    return date;
+  },
+};
