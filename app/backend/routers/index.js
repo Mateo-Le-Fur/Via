93d46b53db9e2453ad929185147202767e09cf71
Mateo-Le-Fur <mateo.le-fur@hotmail.com>
2022-08-26 17:40:21 +0200

    update

--- app/backend/routers/index.js
+++ app/backend/routers/index.js
@@ -13,7 +13,7 @@ router.use('/auth', auth);
 router.use('/user', authJWT.protect, user);
 router.use('/activity', authJWT.protect, activity);
 router.use('/dashboard', authJWT.protect, adminCheck.protect, dashboard);
-router.use('/current', authJWT.protect, controllerHandler(userController));
+router.use('/current', authJWT.protect, controllerHandler(userController.getCurrentUser));
 router.use('/docs', authJWT.protect, adminCheck.protect, apiDoc);
 
 module.exports = router;
