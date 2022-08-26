fd21f02a864896ba211aaaf2cbf02503f9be292b
Mateo-Le-Fur <mateo.le-fur@hotmail.com>
2022-08-26 17:27:40 +0200

    bracket

--- app/client/src/components/Prorfile/Profile.jsx
+++ app/client/src/components/Prorfile/Profile.jsx
@@ -89,7 +89,7 @@ const Profile = () => {
         method: "POST",
         body: name,
       });
-    } catch (error) {}
+    } catch (error) { }
   }
 
   async function getUserAvatar(id) {
@@ -112,132 +112,133 @@ const Profile = () => {
   };
 
 
-if(user){
+  if (user) {
 
 
-  return (
+    return (
 
-    <div className="profile">
-      {isError && message && <p className="server-error">{message}</p>}
-      <form className="editForm" onSubmit={handleSubmit}>
-        <div className="avatar">
-          <input
-            type="file"
-            id="avatar"
-            onChange={handleAvatar}
-            name="avatar"
-          />
-          <label htmlFor="avatar">
-            <img src={avatar} alt="avatar" />
-          </label>
-        </div>
-        <div
-          className={
-            form.firstname.length > 0 ? "field field--has-content" : "field"
-          }
-        >
-          <input
-            className="field-input"
-            name="firstname"
-            type="text"
-            id="firstname"
-            value={form.nickname}
-            placeholder="Prénom"
-            onChange={handleChange}
-          />
-          <label className="field-label" htmlFor="firstname">
-            Prénom
-          </label>
-        </div>
-        <div
-          className={
-            form.lastname.length > 0 ? "field field--has-content" : "field"
-          }
-        >
-          <input
-            value={form.lastname}
-            name="lastname"
-            className="field-input"
-            type="text"
-            id="lastname"
-            placeholder="Nom"
-            onChange={handleChange}
-          />
-          <label htmlFor="lastname" className="field-label">
-            Nom
-          </label>
-        </div>
-        <div
-          className={
-            inputAddress.length > 0
-              ? "field field--has-content field-address"
-              : "field field-address"
-          }
-        >
-          <input
-            value={inputAddress}
-            className="field-input"
-            type="text"
-            id="address"
-            placeholder="Adresse"
-            onChange={handleChangeAddress}
-          />
-          {showSuggestionBox && (
-            <SuggestionBox
-              inputAddress={inputAddress}
-              handleAddress={handleAddress}
+      <div className="profile">
+        {isError && message && <p className="server-error">{message}</p>}
+        <form className="editForm" onSubmit={handleSubmit}>
+          <div className="avatar">
+            <input
+              type="file"
+              id="avatar"
+              onChange={handleAvatar}
+              name="avatar"
             />
-          )}
-          <label htmlFor="lastname" className="field-label">
-            Adresse
-          </label>
-        </div>
-        <div
-          className={
-            form.phone.length > 0 ? "field field--has-content" : "field"
-          }
-        >
-          <input
-            value={form.phone}
-            name="phone"
-            className="field-input"
-            type="text"
-            id="phone"
-            placeholder="Téléphone"
-            onChange={handleChange}
-          />
-          <label htmlFor="phone" className="field-label">
-            Téléphone
-          </label>
+            <label htmlFor="avatar">
+              <img src={avatar} alt="avatar" />
+            </label>
+          </div>
+          <div
+            className={
+              form.firstname.length > 0 ? "field field--has-content" : "field"
+            }
+          >
+            <input
+              className="field-input"
+              name="firstname"
+              type="text"
+              id="firstname"
+              value={form.nickname}
+              placeholder="Prénom"
+              onChange={handleChange}
+            />
+            <label className="field-label" htmlFor="firstname">
+              Prénom
+            </label>
+          </div>
+          <div
+            className={
+              form.lastname.length > 0 ? "field field--has-content" : "field"
+            }
+          >
+            <input
+              value={form.lastname}
+              name="lastname"
+              className="field-input"
+              type="text"
+              id="lastname"
+              placeholder="Nom"
+              onChange={handleChange}
+            />
+            <label htmlFor="lastname" className="field-label">
+              Nom
+            </label>
+          </div>
+          <div
+            className={
+              inputAddress.length > 0
+                ? "field field--has-content field-address"
+                : "field field-address"
+            }
+          >
+            <input
+              value={inputAddress}
+              className="field-input"
+              type="text"
+              id="address"
+              placeholder="Adresse"
+              onChange={handleChangeAddress}
+            />
+            {showSuggestionBox && (
+              <SuggestionBox
+                inputAddress={inputAddress}
+                handleAddress={handleAddress}
+              />
+            )}
+            <label htmlFor="lastname" className="field-label">
+              Adresse
+            </label>
+          </div>
+          <div
+            className={
+              form.phone.length > 0 ? "field field--has-content" : "field"
+            }
+          >
+            <input
+              value={form.phone}
+              name="phone"
+              className="field-input"
+              type="text"
+              id="phone"
+              placeholder="Téléphone"
+              onChange={handleChange}
+            />
+            <label htmlFor="phone" className="field-label">
+              Téléphone
+            </label>
 
+          </div>
+          <div className="areaContainer">
+            <textarea
+              onChange={handleChange}
+              value={form.description}
+              name="description"
+              id="description"
+              placeholder="Description"
+            ></textarea>
+          </div>
+          <div className="buttonContainer">
+            <button type="submit" className="btn">
+              Enregistrer
+            </button>
+          </div>
+        </form>
+        <h2>Mes activités</h2>
+        <div className="activityList">
+          {filtered.length > 0 ? (
+            filtered.map((activity) => (
+              <Card type="profile" activity={activity} key={activity.id} />
+            ))
+          ) : (
+            <h2>Vous n'avez pas encore créé d'actvitiés</h2>
+          )}
         </div>
-        <div className="areaContainer">
-          <textarea
-            onChange={handleChange}
-            value={form.description}
-            name="description"
-            id="description"
-            placeholder="Description"
-          ></textarea>
-        </div>
-        <div className="buttonContainer">
-          <button type="submit" className="btn">
-            Enregistrer
-          </button>
-        </div>
-      </form>
-      <h2>Mes activités</h2>
-      <div className="activityList">
-        {filtered.length > 0 ? (
-          filtered.map((activity) => (
-            <Card type="profile" activity={activity} key={activity.id} />
-          ))
-        ) : (
-          <h2>Vous n'avez pas encore créé d'actvitiés</h2>
-        )}
       </div>
-    </div>
-  );
-};
+    );
+  };
+}
 
 export default Profile;
