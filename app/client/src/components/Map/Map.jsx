e0b1ef47174e8d29de519fdcf8a1d2ebfa0c51a5
LouisLeca22 <louisleca@zohomail.eu>
2022-09-01 16:42:04 +0200

    landing page

--- app/client/src/components/Map/Map.jsx
+++ app/client/src/components/Map/Map.jsx
@@ -1,9 +1,9 @@
 import { MapContainer, TileLayer, ZoomControl } from 'react-leaflet'
 import "./Map.scss";
 
-const Map = ({center, children, zoom, zoomControl}) => {
+const Map = ({center, children, zoom, zoomControl, blur}) => {
   return (
-    <MapContainer center={center} zoomControl={false} zoom={zoom} scrollWheelZoom={true} attributionControl={false}>
+    <MapContainer center={center} zoomControl={false} zoom={zoom} scrollWheelZoom={true} attributionControl={false} className={blur ? "map-blur" : ""}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- app/client/src/pages/Landing/Landing.jsx
+++ app/client/src/pages/Landing/Landing.jsx
@@ -2,13 +2,15 @@ import "./Landing.scss";
 import Map from "../../components/Map/Map"
 import LandingForm from "./LandingForm"
 import "./Landing.scss";
+import Banner from "./Banner";
 
 const Landing = () => {
   
   return (
     <div className='landing'>
-      <Map zoom={13} center={[48.86, 2.33]} />
+      <Map zoom={13} center={[48.86, 2.33]} blur={true} />
       <LandingForm />
+      <Banner />
     </div>
   )
 }
