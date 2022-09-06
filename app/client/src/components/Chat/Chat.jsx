9813f1c92e2dff02064e35530f218e1ee0d2ac69
LouisLeca22 <louisleca@zohomail.eu>
2022-09-06 10:52:23 +0200

    fix messages + via

--- app/client/src/components/Chat/Chat.jsx
+++ app/client/src/components/Chat/Chat.jsx
@@ -5,7 +5,32 @@ import {FaPaperPlane} from "react-icons/fa"
 import { useSelector } from "react-redux"
 const Chat = () => {
 
-
+  // const messages = [
+  //   {
+  //     id: "1",
+  //     isMine: true,
+  //     avatar: "https://randomuser.me/api/portraits/men/57.jpg",
+  //     message: "hello",
+  //     user: "Ypm",
+  //     date: "22/20/2002"
+  //   },
+  //   {
+  //     id: "2",
+  //     isMine: false,
+  //     avatar: "https://randomuser.me/api/portraits/men/57.jpg",
+  //     message: "hello",
+  //     user: "Ypm",
+  //     date: "22/20/2002"
+  //   },
+  //   {
+  //     id: "3",
+  //     isMine: true,
+  //     avatar: "https://randomuser.me/api/portraits/men/57.jpg",
+  //     message: "hello",
+  //     user: "Ypm",
+  //     date: "22/20/2002"
+  //   },
+  // ]
 
    const {messages, user} = useSelector(state => state.auth)
   return (
@@ -17,6 +42,9 @@ const Chat = () => {
         ): (
             <p>Vous n'avez pas encore de message</p>
         )}
+        {/* {messages.map(message => (
+          <Message key={message.id} mine={message.isMine} message={message}/>
+        ))} */}
   
     </div>
   )
